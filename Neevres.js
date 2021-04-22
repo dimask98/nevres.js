b579c82c436ef3ea8bd2de71161b1d9a0e9f32e2
dimask98 <56661641+dimask98@users.noreply.github.com>
2021-04-22 04:39:15 +0300

    Create Neevres.js

---
+++ Neevres.js
@@ -0,0 +1,76 @@
+$(`<img src="sico/z1f3o8d8tj10.png" class=".lonline.light.break" style="position: absolute;
+    transform: matrix(2, 0, 0, 1, 1, 11);
+    margin-top: 70px;
+    right: 40px;
+    background-color: #ffffff00;
+    width: 50px;">
+`).insertBefore('.lonline.light.break');
+$(`<img src="sico/z1f3o7s2kk10.png" class=".lonline.light.break" style="transform: matrix(2, 0, 0, 1, 0, 100);
+    margin-top: 50px;
+    right: 30px;
+    position: absolute;
+    width: 45px;">
+`).insertBefore('.lonline.light.break');vchat=true;
+
+`).insertBefore('.nav-tabs');$('.nav-tabs').addClass('fl').css('width','121%'); $('.nav-tabs').addClass('fl').css('width','100%'); $('.nav-tabs').addClass('fl').css('width','100%');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+
+#as {font-family: 'jazeera-light',FontAwesome;margin-bottom: 2px; color: #A68F81; border-radius: 5px 5px 5px 5px;
+    text-shadow: 2px 2px 2px rgb(138, 138, 138) , 0px 2px 10px #A68F81 !important;
+border: 1px solid #A68F81;
+height: 33px;
+text-align: center; 
+}
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 15px!important;text-align: center!important;width: 101%;color: #A68F81;white-space: nowrap;border-radius: 14px 14px 14px 14px;}
+.border {shadow:1px 1px 1px 1px ridge rgb(137, 137, 137); box-shadow:1px 1px 1px 1px rgb(137, 137, 137) }
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);border-radius: 5px 5px 5px 5px;;}
+ul.nav.nav-tabs.fl li a {      color: #A68F81;  box-shadow: inset 0 3px 3px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);font-family: 'jazeera',FontAwesome;font-size: 12px!important;padding: 5px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 2px solid rgb(138, 137, 137);width: 33.3%;}
+textarea.fl.filw.corner.tbox {    box-shadow:inset 0 0px 0px rgb(137, 137, 137); 0 0 7px rgb(137, 137, 137);border-radius: 7px;padding-left: 5px;}
+label.label.tc.border.btn.fl {border-radius: 41px 21px 41px 21px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 1px solid #A68F81;font-family: 'jazeera',FontAwesome;border-radius: 4px 4px 4px 4px;}
+label.fl.nosel.label.pnhead {font-family: 'jazeera',FontAwesome;width: 81%;text-align: -webkit-center;background-color: #A68F81;color: white;border-radius:13px 13px 13px 13px;}
+.fr.borderg a {color: #A68F81;font-size: 12px!important;}
+.fr.borderg {color: #A68F81;font-size: 13px!important;border: 1px solid #A68F81;font-family: 'jazeera',FontAwesome;border-radius: 4px 4px 4px 4px;margin-top: 1px;}
+label.label-primary {border-radius: px 31px 31px 31px;}
+button.btn.fr.btn-success.fa.fa-refresh {
+padding: 4px 5px;
+background-color:"url()"});
+background-image::"url()"});
+border: 1px solid transparent;
+margin-top: -3px;
+border-radius: 50px;
+as:51px;
+}
+if(location.protocol!='https:'){location.protocol='https:';}
+
+.border.corner, .border.corner {box-shadow: inset 0 0px 0px rgb(137, 137, 137), 0 0 4px rgb(137, 137, 137);border: 2px solid #A68F81;as:13px;}
+.label-primary, .btn-primary, .ae.fa.label.fa-commenting-o.label-warning, .ae.fa.chats.label.fa-comment.label-warning {border: 1px solid #A68F81;border-radius: 31px 31px 31px 31px;as:31px;}
+.checkbox {color: rgb(137, 137, 137);}
+
+</style>`).insertBefore('body');
+$('.u-msg.dots').removeClass('dots');
+$("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+   $(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://trrrrf.com/sico/z1f3ebjvcs10.jpeg);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="https://www.m6e.net/sico/z1f1qjeo9k10.gif"    >
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">شات ترف للجوال</span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك 👋<h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1>    نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله في شات  ترف 
+ </div>
+      </div>
+    </div></div>
+    `).appendTo('div#d2')
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+})
+$(".fa-gear").text("الاعدادات").css("width","");
+[4/22, 4:36 AM] نورس: eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(\'#1,#2,#3\').4(\'0\');$(\'.0\').5("6",7);',8,8,'ali|u2|u3|u1|addClass|attr|maxlength|8'.split('|'),0,{}))
