c28b8417e58ce35bfc1c6d1b4ca9a6808043c83f
dimask98 <56661641+dimask98@users.noreply.github.com>
2021-05-01 06:23:57 +0300

    Create Watin.js

---
+++ Watin.js
@@ -0,0 +1,55 @@
+setTimeout (function (){   $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'> استايلات: <a href='https://api.whatsapp.com/send?phone=905382743878&text=&source=&data='>نورس السوري</a></div>").insertAfter($(".loginstat"));  },1000);
+
+$(` <a id="des3" href="https://watin2.com/" style="border-radius: 5px 0px 5px 0px;width:23%;margin:2px;" class="fl mini  fa  fa-
+ btn btn-primary">التطبيق</a>
+<a id="des3" href="https://watin2.com/" style="border-radius: 5px 0px 5px 0px;width:23%;margin:2px;" class="fl mini  fa   fa-
+ btn btn-default">الاشتراك</a>
+<a id="des3" href="https://watin2.com/" style="border-radius: 5px 0px 5px 0px;width:27%;margin:2px;" class="fl mini  fa fa-
+btn btn-primary">قوانين الموقع</a>
+<a id="des3" href="https://watin2.com/" style="border-radius: 5px 0px 5px 0px;width:22%;margin:2px;" class="fl mini  fa fa-
+btn btn-default">اتصل بنا</a>
+ `).insertBefore('.nav-tabs');
+ $("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+   $(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://watin2.com/sico/1619836211051.jpg);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="https://watin2.com/sico/1616770218124.gif"    >
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">   شات  وتين  للجوال</span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك 👋<h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1>    نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله في شات وتين </div>
+      </div>
+    </div></div>
+    `).appendTo('div#d2')
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+})
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$.getScript("https://cdn.statically.io/gh/dimask98/nevres.js/main/Hoobbii.js");
+
+$('.hand.nosel.fl.uzr.border').css({'margin': '0px',borderRight: '3px solid #316C7D',borderLeft: '3px solid #316C7D',borderTop: '1px solid #e2e1e1',borderBottom: '1px solid #e2e1e1',borderRadius: '3px'});
+$(`<center><div><div width="75.5%" style="background-color: #b7c3c5; color: #BF4A47;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;"  
+><font style="background-color: #fff;border-radius: 0px 25px 0px 25px;font-family: 'Cairo', sans-serif, FontAwesome;padding: 2px 10px 2px 10px; margin:0px 25px 0px 25px;" >اهلآ وسهلاً بكم فِي شآت   وتين للجوال</font></div></div><center>`).insertBefore('#d2');
+
+$('.break.light').css({"background-image":"url(https://almhapa.com/bnr.jpg)"});
+(document.getElementById("tbox").placeholder = "  مـآ يلـفظ من قـول الا لـديه رقـيب عتيـد ");
+$(`<button style="width:24px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-microphone fl btn btn-info"></button>`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+$
+$(` <center><div width="99%" id="asim7"><font style="background-color:#FFFFFF;color:#BF4A47;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > 
+ ((  دردشة الحائط ممنوعه))  </font></div><center>`).insertBefore('div#d2bc');
+
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -9px;font-family: 'Changa', sans-serif;    height: 20px;color: #;"  onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"> مُرحب بالجميع في  شات وتين المحبه اكبر تجمع عربي هذه سَماء ذاتُ سَعة و امْتِداد ،و أنتم نجومها. نحن  المميزون والباقي المقلدون  )
+</marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+
+$(".fa-gear").text("⚙️الاعدادات").css("width","");
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<a class="fl mini fa fa-bullhorn  btn btnn-default"></a><input type="jfa-w" name="FirstName" value="الشيخ خالد الجليل"><audio src="https://chatpalestine.me/SaHeR3.mp3" controls loop></audio><input type="jfa-w" name="FirstName" value="سورة الحجر"><audio src="https://chatpalestine.me/SaHeR2.mp3" controls loop></audio><input type="jfa-w" name="FirstName" value="سورة النمل"><audio src="https://chatpalestine.me/SaHeR1.mp3" controls loop></audio></div><center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)});$
+$("div#l3 button.btn.btn-primary").bind("click",function(){alert("عند تسجيلك للعضوية فانك توافق علا قوانين  وتين واي اختلال في القوانين سيتم حظرك وحذف العضوية");})
+
+$("div#l2 button.btn.btn-primary").bind("click",function(){alert("اهلا وسهلا بكم في عائلتنا عائلة شات وتين المحبه  ");})
