be4ccc5987c5f9cf00312df7c4a0122e15ddc760
dimask98 <56661641+dimask98@users.noreply.github.com>
2021-05-07 23:24:57 +0300

    Create Bstni.js

---
+++ Bstni.js
@@ -0,0 +1,57 @@
+setTimeout (function (){   $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>استايلات : <a href='https://api.whatsapp.com/send?phone=905382743878&text=مرحبا-نورس-السوري&source=&data=&app_absent='>   نورس السوري</a></div>").insertAfter($(".loginstat"));  },1000);
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> ( حياكم الله في شات  بستوني للجوال ) َ</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+
+
+$(".ubnr").remove();
+if(location.protocol!='https:'){location.protocol='https:';} 
+$(`<a id="des3" href="" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa  fa-graduation-cap btn btn-primary"> الفعاليات</a>
+<a id="des3" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star btn btn-default" href="https://bnatdfh.com/">القوانين</a>
+<a id="des3" style="border-radius: 24px;width:27%;margin:2px;" class="fl mini  fa fa-graduation-cap btn btn-primary" href="https://bnatdfh.com/">الإشتركات  
+</a>
+<a id="des3" style="border-radius: 24px;width:22%;margin:2px;" class="fl mini  fa fa-phone btn btn-default" href="https://bnatdfh.com/">الرئيسية</a>
+
+<div style=" width: 100%; "  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">         اداره الموقع           </div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  &nbsp;<i class="fa fa-paper-plane">  &nbsp;</i> اداره الموقع </p>     <i class="fa fa-comment"></i> <i class="fa fa-share"> </i> </p>    <i class="fa fa-comment"></i> <i class="fa fa-share">    ‏ | جوكر فلسطين    </i> </p>  <i class="fa fa fa-comment"></i> <i class="fa fa-share">     </i>          </div></div><center></div></div><center><center><div><marquee direction="right" wi
+
+`).insertBefore('.nav-tabs');
+
+
+
+
+
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span>لوحة تحكم شات  بستوني ")
+
+$(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome"> إعلانات شات  بستوني للضروره فقط </div>`).insertBefore('span.fl.fa.fa-send');
+
+
+
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><input type="jfa-w" name="FirstName" value=" قرآن كريم ">
+<audio class="wp-audio-shortcode" id="audio-236-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="https://h.top4top.io/m_1595scph70.mp3"><a href="https://h.top4top.io/m_1595scph70.mp3">https://h.top4top.io/m_1595scph70.mp3</a></audio>
+
+<center></center></center>
+
+</div>`);
+
+$('#d0').append(`<label title="إزاعة" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
+data-toggle="tab" data-target="#mic"  class="ae fa label label-primary fa-microphone ">إزاعة</label>`);
+
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="jfa-w" name="FirstName" value=" الأخبــــار "><iframe class="fr" style="width:100%;margin-top: 0px;" src="https://www.bbc.com/ws/av-embeds/media/bbc_arabic_radio/liveradio/ar" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><center>`).appendTo
+("#mic"),clearInterval(b)):console.log(c)},1)});
+
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="jfa-w" name="FirstName" value=" أجنبـــــي "><audio src="https://d.top4top.io/m_1673vuf0x1.mp4" controls loop></audio></div><center>`).appendTo
+("#mic"),clearInterval(b)):console.log(c)},1)});
+
+
+("[data-target=#wall]").attr('title',' " حائط بستوني ، للأبدآع .. والمسابقات ..   ')
+uf[ gb ]=uf[ uk ];
+
+
+
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 15px!important;text-align: center!important;width: 101%;color: #;white-space: nowrap;border-radius: 14px 14px 14px 14px;}
+.border {shadow:1px 1px 1px 1px ridge rgb(137, 137, 137); box-shadow:1px 1px 1px 1px rgb(137, 137, 137) }
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);border-radius: 5px 5px 5px 5px;;}
+.border.corner, .border.corner {box-shadow: inset 0 0px 0px rgb(137, 137, 137), 0 0 4px rgb(137, 137, 137);border: 2px solid #ffffff;as:13px;}
+
+</style>`).insertBefore('body')
