57e7ddf79ce7121ad4763a0d171d7e290994ba87
dimask98 <56661641+dimask98@users.noreply.github.com>
2021-05-08 06:44:56 +0300

    Create Dimashki7lm.js

---
+++ Dimashki7lm.js
@@ -0,0 +1,263 @@
+$(`${`<form name="jump"> 
+ <div class="MyColors"> 
+ 					<map id="ColorsMap" name="ColorsMap"> 
+ 					<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle('none',60)" shape="rect" title="1"> 
+ 					<area alt="" coords="244, 0, 266, 25" href="javascript:chooseStyle('Co2',60)" shape="rect" title="2"> 
+ 					<area alt="" coords="212, 0, 233, 25" href="javascript:chooseStyle('Co3',60)" shape="rect" title="3"> 
+ 					<area alt="" coords="179, 0, 201, 25" href="javascript:chooseStyle('Co4',60)" shape="rect" title="4"> 
+ 					</map> 
+ 			 <img alt="" height="40" src="https://1.top4top.net/p_1388gqx1c1.gif" width="300" usemap="#ColorsMap"></div> 
+ </form> 
+ 
+ 
+ 
+ 
+ 
+ `}`).insertBefore('.nav-tabs'); 
+
+$.getScript('https://cdn.statically.io/gh/dimask98/nevres.js/main/7lm.js'); 
+
+$("<div class='fr borderg' style='padding:2px;background-color:white;float:left;'> استايلات  :<a href='https://api.whatsapp.com/send?phone=905382743878&text=مرحبا-نورس-السوري&source=&data=&app_absent='> نورس السوري</a></div>").insertAfter($('.loginstat')); 
+
+$('.hand.nosel.fl.uzr.border').css({ 'margin': '1px 0', paddingTop: '2px', borderRadius: '50px 50px 50px 50px' }); 
+
+$('.uzr.fl.corner.borderg').css({ 'margin': '0px 0', paddingTop: '2px', borderRadius: '5px' }); 
+
+$('.checkbox label').html(`${'<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">'}`); 
+$('.nav-tabs').addClass('fl').css('width', '100%'); 
+
+$(`${`<link rel="stylesheet" type="text/css" href=""> 
+ <link href="https://cdn.statically.io/gh/dimask98/nevres.js/main/7lm1.css" rel="stylesheet" type="text/css" /> 
+ <link href="https://cdn.statically.io/gh/dimask98/nevres.js/main/7lm2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" /> 
+ <link href="https://cdn.statically.io/gh/dimask98/nevres.js/main/7lm3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" /> 
+ <link href="https://cdn.statically.io/gh/dimask98/nevres.js/main/7lm4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" /> 
+ <style> 
+ 
+ 
+ 
+ a.label.label-primary.fl { 
+ 
+ margin: 2px -127px!important; 
+ 
+ } 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ img.fitimg { 
+ border-radius: 25px; 
+ } 
+ 
+ 
+ 
+ 
+ 
+ fl.u-pic { 
+ border-radius: 20px !important; 
+ } 
+ 
+ button.btn.fr.btn-success.fa.fa-refresh { 
+ display: none; 
+ } 
+ 
+ a.label.label-primary.fl img.fl {display: none;} 
+ 
+ a.label.label-primary.fl { 
+ border-radius: 25px!important; 
+ margin: 2px -252px; 
+ padding: 4px!important; 
+ margin-bottom: 2px; 
+ text-align: right!important; 
+ position: absolute; 
+ } 
+ 
+ div#l2 br,div#l3 br { 
+ display: none 
+ } 
+ 
+ #des22 { 
+ text-align: center!important; 
+ font-size: 100%!important; 
+ } 
+ label.fl.label.loginstat { 
+ background-color: transparent!important; 
+ position: absolute; 
+ top: 0; 
+ margin-top: 4px; 
+ left: 0; 
+ } 
+ .fr.borderg, .fr.borderg a { 
+ font-size: 10px!important; 
+ padding: 0 5px!important; 
+ position: relative; 
+ color: #ffffff; 
+ } 
+ .fr.borderg { 
+ border: 0px; 
+ margin-top: 0; 
+ height: 0; 
+ 
+ } 
+ span.s1.fa.fa-user.label.badgex.label-as-badge.label-success { 
+ padding: 0px 3px 2px; 
+ margin-right: 5px; 
+ } 
+ label.label.label-primary.mini.fl { 
+ padding: 1px; 
+ border-radius: 15px!important; 
+ } 
+ 
+ 
+ 
+ 
+ div#l1 input#u1,div#l3 button { 
+ width: 99%; 
+ margin-left: 1px 
+ } 
+ 
+ div#l2 button,div#l3 button { 
+ border-radius: 24px!important; 
+ height: 27px; 
+ 	padding: 0px 5px; 
+ } 
+ 
+ div#l1 input#u1 { 
+ border-radius: 15px; 
+ border: 1px solid #e1e0e0; 
+ margin-top: 3px; 
+ text-align: center 
+ } 
+ 
+ div#l2 input#pass1,div#l2 input#u2,div#l3 input#pass2,div#l3 input#u3 { 
+ width: 49%; 
+ padding: 0!important; 
+ float: right; 
+ margin: 2px 5px 2px 0 
+ } 
+ 
+ div#l2 input#u2,div#l3 input#u3 { 
+ margin-left: -6px; 
+ border-radius: 15px; 
+ border: 1px solid #e1e0e0; 
+ text-align: center 
+ } 
+ 
+ div#l2 input#pass1,div#l3 input#pass2 { 
+ border-radius: 15px; 
+ border: 1px solid #e1e0e0; 
+ text-align: center 
+ } 
+ 
+ div#l2,div#l3 { 
+ } 
+ 
+ div#l1 { 
+ margin: 0 1px -18px; 
+ } 
+ 
+ div#l1,div#l2,div#l3 { 
+ border-bottom: 2px solid; 
+ margin-bottom: 2px; 
+ border-top: 2px solid; 
+ margin-top: 0; 
+ border-color: #bababa; 
+ border-radius: 15px; 
+ height: 69px 
+ } 
+ 
+ div#l1 button { 
+ 
+ border-radius: 24px!important; 
+ width: 99%; 
+ height: 27px; 
+ padding: 0px 5px; 
+ } 
+ 
+ div#l2 button { 
+ width: 70% 
+ } 
+ 
+ ul.nav.nav-tabs>li>a { 
+ border: none; 
+ text-align: center; 
+ border-radius: 3px; 
+ display: initial; 
+ width: 80%; 
+ float: left; 
+ padding: 0!important; 
+ } 
+ 
+ ul.nav.nav-tabs>li.active>a { 
+ border: none; 
+ border-radius: 3px; 
+ } 
+ 
+ ul.nav.nav-tabs>li { 
+ width: 32%; 
+ margin: 0 5px 0 0; 
+ } 
+ 
+ ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before { 
+ float: right; 
+ margin-right: -26%; 
+ padding: 0 2px 1px; 
+ border-radius: 3px; 
+ width: 23px; 
+ text-align: center; 
+ } 
+ 
+ 
+ 
+ .nav>li>a:focus,.nav>li>a:hover { 
+ color: #9abff9; 
+ } 
+ 
+ ul.nav.nav-tabs { 
+ margin-top: 2px; 
+ float: right; 
+ width: 100%; 
+ padding: 3px 1px; 
+ } 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ #des0 { 
+ transform: matrix(1, 0, 0, 1, 126, -124); 
+ border-radius: 15px 15px 15px 15px; 
+ position: absolute; 
+ width: 79% !important; 
+ height: 19px; 
+ left: -50px; 
+ font-size: 14px !important; 
+ color: #fff; 
+ padding: 0px 0 !important; 
+ } 
+ 
+ 
+ .des2 { 
+ background-color: #fff; 
+ border-radius: 0; 
+ padding: 2px 10px; 
+ margin: 3px 8px!important; 
+ 	border-radius: 15px!important; 
+ } 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ </style>`}`).insertBefore('body')
