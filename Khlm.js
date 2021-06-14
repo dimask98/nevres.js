19d22e7427e988102a6f2b294fabd888173c23e6
dimask98 <56661641+dimask98@users.noreply.github.com>
2021-06-14 17:44:14 +0300

    Create Khlm.js

---
+++ Khlm.js
@@ -0,0 +1,32 @@
+$('.u-msg.dots').removeClass('dots');$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+
+#as {font-family: 'jazeera-light',FontAwesome;margin-bottom: 2px; color: #665E66; border-radius: 5px 5px 5px 5px;
+    text-shadow: 2px 2px 2px rgb(138, 138, 138) , 0px 2px 10px #665E66 !important;
+border: 1px solid #665E66;
+height: 33px;
+text-align: center; 
+}
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 15px!important;text-align: center!important;width: 101%;color: #665E66;white-space: nowrap;border-radius: 14px 14px 14px 14px;}
+.border {shadow:1px 1px 1px 1px ridge rgb(137, 137, 137); box-shadow:1px 1px 1px 1px rgb(137, 137, 137) }
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);border-radius: 5px 5px 5px 5px;;}
+ul.nav.nav-tabs.fl li a {      color: #665E66;  box-shadow: inset 0 3px 3px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);font-family: 'jazeera',FontAwesome;font-size: 12px!important;padding: 5px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 2px solid rgb(138, 137, 137);width: 33.3%;}
+textarea.fl.filw.corner.tbox {    box-shadow:inset 0 0px 0px rgb(137, 137, 137);
+</style>`).insertBefore('body');$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 15px!important;text-align: center!important;width: 101%;color: #;white-space: nowrap;border-radius: 14px 14px 14px 14px;}
+.border {shadow:1px 1px 1px 1px ridge rgb(137, 137, 137); box-shadow:1px 1px 1px 1px rgb(137, 137, 137) }
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);border-radius: 5px 5px 5px 5px;;}
+.border.corner, .border.corner {box-shadow: inset 0 0px 0px rgb(137, 137, 137), 0 0 4px rgb(137, 137, 137);border: 2px solid #ffffff;as:13px;}
+
+</style>`).insertBefore('body')
+$( "button.btn.btn-primary" ).css( "border-radius", "15px 15px 15px 15px" )
+$(".label-primary").css("border-radius","15px 15px 15px 15px")
+$ ( ` <center><div style =" width: 100?؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample">- ¦¦ - نتمنا لگم اجمل الاوقات | شات حلم  - ¦¦ -</ div><div class = "" id = "collapseExample"><center></ i> </ div> <center> ` ). insertBefore ( '#d2' );
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif;    height: 20px;color: #FFFFFF;"  onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">مرحبا بكم في شات حلم أتمنى أن تكونو سعداء وأن تقضوا أجمل أوقاتكم معنا</marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success')
