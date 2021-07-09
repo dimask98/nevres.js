ba8d88945bf672f0065ebbff4ad3410a364d26a9
dimask98 <56661641+dimask98@users.noreply.github.com>
2021-07-09 10:56:56 +0300

    Create Hanin.js

---
+++ Hanin.js
@@ -0,0 +1,104 @@
+$(`<img src="https://salam0.com/sico/1625816810888.jpg" style="width:100%;margin-top: 0px" ><center><div id="asim13"><a id="asim21" href="About-Us.html" style="height: 24px;text-align: center;width:124px;color: #fffff;border-radius: 15px 0px 0px 0px; border-left: 0px solid#f7d322; border-right: 1px solid#ffffff;" class="fl mini fa fa-coffee btn btn-primary"/a><a id="asim21" href="style2.html" style="height: 24px;text-align: center;width:70px;color: #fffff;border-radius: 0px 0 0 0; border-left: 0px solid#f7d322; border-right: 1px solid#ffffff;" class="fl mini fa fa-tencent-weibo btn btn-primary" target="_blank"></a><a id="asim21" href="Contact-Us.html"  style="height: 24px;text-align: center;width:70px;color: #fffff;border-radius: 0px 0 0 0; border-left: 0px solid#f7d322; border-right: 1px solid#ffffff;" class="fl mini fa fa-exclamation-triangle btn-primary" target="_blank"></a></a><a id="asim21" href="" style="height: 24px;text-align: center;width:124px;color: #fffff;border-radius: 0px 15px 0px 0px; border-left: 0px solid#f7d322; border-right: 1px solid#ffffff;" class="fl mini fa fa-envelope btn btn-primary" target="_blank"></a></a></div></div><center>`).insertBefore(".nav-tabs"),$(`<div><marquee direction="right"id="as43"scrollamount="3">اهلا وسهلا بكم في شات الحنين للجوال حياكم الله جميعا</marquee><div>`).insertBefore("div#tlogins .lonline.light.break");$(".hand.nosel.fl.uzr.border").css({margin:"1px 0",borderRadius:"0px"}),$(".checkbox label").html(`<input id="stealth" type="checkbox" value=""><i class="fa fa-eye-slash" aria-hidden="true"></i>`),$(".nav-tabs").addClass("fl").css("width","100%"),$("div#tlogins input").attr("maxlength",12),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",100),
+$(`<link rel="stylesheet" href="https://cdn.statically.io/gh/dimask98/nevres.js/main/Damascus.css"><style> div#asim13 {
+    background: #fff0;
+}
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before{color:#c1958a}a#asim21,div#tlogins button.btn.btn-primary,label.label.label-primary.mini.fl{background-color:#ab9bb5!important}ul.nav.nav-tabs>li>a{background-color:#ab9bb5}ul.nav.nav-tabs>li.active>a{background-color:#ab9bb5}.fr.borderg{background-color:#ab9bb5!important}.loginstat{background-color:#ab9bb5}label.fl.nosel.label.pnhead{background-color:#ab9bb5}
+div#tlogins .fade {
+    opacity: 1!important;
+    -webkit-transition: none!important;
+    -o-transition: none!important;
+    transition: none!important;
+    border-left: 1px solid #ab9bb5;
+    border-right: 1px solid #ab9bb5;
+    background-image: url(zzzzzz);
+    background-position-y: center;
+    background-size: cover;
+    background-origin: content-box;
+}
+
+div#l1 {
+    height: 84px;
+}
+.fl.fa.fa-sign-in.btn.btn-primary.dots.roomh.border.corner {
+    background-color: #c11616!important;
+    padding: 1px 4px!important;
+    pointer-events: none;
+}
+button.fa.fa-send.fl.btn.btn-primary {
+    margin-top: -0.1px!important;
+    border-radius: 10%;
+}
+
+button.fa.fa-send.fl.btn.btn-primary {
+    /* margin-top: 0!important; */
+}
+.fa.fa-send.fl.btn.btn-primary {
+    background-color: #c11616!important;
+    color: #fff!important;
+}
+.tbox {
+    padding: 3px;
+    min-height: 28px!important;
+    border-right: 5px solid;
+    border-right-color: currentcolor;
+    border-right-style: solid;
+    border-left: 5px solid;
+    border-left-color: currentcolor;
+    border-left-style: solid;
+    border-color: #d2d2d2;
+    border-left-style: groove;
+    border-right-style: ridge;
+    border-radius: 3px!important;
+}
+a.btn.minix.fa.fa-times.fr, a.btn.minix.fa.fa-thumbs-up.fr, a.btn.minix.fa.fa-comment.fr {
+    border-left: 1px solid #d3d3d3;
+    border-top: 1px solid #d3d3d3;
+    border-right: 1px solid #a6a6a6;
+    border-bottom: 1px solid #a6a6a6;
+    border-radius: 3px;
+}
+a.btn.minix.fa.fa-comment.fr {
+    color: #fff!important;
+    border-radius: 4px;
+    padding: 1px 4px 2px 5px;
+    margin-top: 6px;
+    background-repeat: repeat-x;
+    border-left: 1px solid #d3d3d3;
+    border-top: 1px solid #d3d3d3;
+    border-right: 1px solid #a6a6a6;
+    border-bottom: 1px solid #a6a6a6;
+    border-radius: 3px;
+    background-image: linear-gradient(to bottom,#5bc0de 0,#2aabd2 100%);
+}
+a.btn.minix.fa.fa-comment:before {
+    content: "\f27a";
+    display: inline!important;
+}
+.fa-comment:before {
+    content: "\f075";
+}
+.label-primary[href]:focus, .label-primary[href]:hover {
+    background-color: #000000;
+}
+</style>`).insertBefore("body");
+$("button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 30px" )
+$(".label-primary").css("border-radius","40px 40px 40px 40px")
+$(document.getElementById("tbox").placeholder = "  المحادثة العامة ");
+$("[data-target=#wall]").attr('title',' فضلاً وليس امراً عدم ألكلام في ألحائط')
+$("[data-target=#settings]").attr('title','تغير صورتك وللونك')
+$("[data-target=#rooms]").attr('title','ألغرف')
+$("[data-target=#chats]").attr('title','السطح  ')
+$("[data-target=#users]").attr('title',' ألأعضاء  ')
+$('.fl.ustat')
+    .css('width', '3px'), $('.fa.fa-user-plus')
+    .html(`❥تسجيل `), document.getElementById('u1')
+    .placeholder = 'اكتب اسمك من 5 احرف فقط ', 
+document.getElementById('pass1')
+  .placeholder = 'كلمة المرور', document.getElementById('u2')
+    .placeholder = 'فخأمة طلتك تنورنا', document.getElementById('pass2')
+    .placeholder = ' ㋡ بـاسوردك ولاتنساه يـ جميل ', document.getElementById('u3')
+    .placeholder = '♥ أكتب اسمك ولازم تحفظه ';
+
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() {
+alert( " جميع الحقوق محفوظة لشات الحنين برعاية الدمشقي " );
+});
