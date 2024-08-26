7ab1c7f186d335081859ceabb822421c72cd1cd1
dimask98 <56661641+dimask98@users.noreply.github.com>
2024-08-26 13:05:15 +0300

    Create ZzzzzzzzzzzzzZ.js

---
+++ ZzzzzzzzzzzzzZ.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <title>حياتسي سيتم اعادة توجيهك</title>
+    <script>
+        window.location.href = "https://ihtc0.com/";
+    </script>
+</head>
+<body>
+    <!-- توصلوا بالسلامة على شات اضم حبك  -->
+</body>
+</html>
+
+<script>
+    window.location.href = "https://ihtc0.com/";
+</script>
