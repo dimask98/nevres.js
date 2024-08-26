16f6a1c3abe064d08c45f30c70f71cfbe74a0bc2
dimask98 <56661641+dimask98@users.noreply.github.com>
2024-08-27 01:49:22 +0300

    Create Tutkceguzel.js

---
+++ Tutkceguzel.js
@@ -0,0 +1,29 @@
+var rid = 'bdu1t0locd';
+
+$('style:eq(0)').append(
+  '#d2 .roomh[onclick="Send_Rjoin(\'upoiguu32r\'' + rid + '\')"]{background-image: linear-gradient(-225deg, #000 0%, #000 29%, #ffd6e0 67%, #3600ff 100%);\nbackground-size: 220% auto;animation: textclip 5s linear infinite;padding: 2px!important;border-radius: 50px !important;border: 1px solid #0000;}'
+);
+$('style:eq(0)').append(
+  '#upro .roomh[onclick="Send_Rjoin(\'upoiguu32r\'' + rid + '\')"]{background-image: linear-gradient(-225deg, #000 0%, #000 29%, #ffd6e0 67%, #3600ff 100%);\nbackground-size: 220% auto;\nanimation: textclip 5s linear infinite;border-radius: 50px !important;border: 1px solid #0000;}'
+);
+$(
+  '<style>\n\n.room.borderg.hand.nosel.fl.rbdu1t0locd.bord.shmok1 {\n  border-left: 0px solid #1e90ff00 !important;\n  border-right: 0px solid #1e90ff00 !important;\n}\n\n.room.borderg.hand.nosel.fl.rmmow5w0ndb .fitimg.u-pic.borderg {\n  letter-spacing: 1px;\n  background-size: 108% 108%;\n  -webkit-text-fill-color: white;\n  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;\n}\n.room.borderg.hand.nosel.fl.rbdu1t0locd .corner.fa.fa-user.label.label-primary.fr.uc {\n  letter-spacing: 1px;\n  -webkit-text-fill-color: #000;\n  border-radius: 15px 0px 15px 0px !important;\n  height: 27px;\n  margin-top: 34px !important;\n  margin-bottom: 0px !important;\n  background-color: #d9534f00!important;\n}\n.room.borderg.hand.nosel.fl.rbdu1t0locd .corner.label.fr.uc.fa.fa-microphone.label-danger {\n  letter-spacing: 1px;\n  -webkit-text-fill-color: #000;\n  border-radius: 15px 0px 15px 0px !important;\n  height: 27px;\n  margin-top: 34px !important;\n  margin-bottom: 0px !important;\n  background-color: #d9534f00!important;\n}\n.room.borderg.hand.nosel.fl.rbdu1t0locd.bord {border-inline: 0px solid #4c121e !important;}\n.room.borderg.hand.nosel.fl.rbdu1t0locd {\nborder-radius: 15px 0px 15px 0px;\ndisplay: inline-block;\nbackground-image: linear-gradient(-225deg, rgb(164, 164, 164) 0%, rgba(255, 0, 143, 0) 29%, rgba(151, 101, 127, 0.04) 67%, rgb(189, 189, 189) 100%);\n}\n.room.borderg.hand.nosel.fl.rbdu1t0locd .fl.mini.u-msg {\n-webkit-text-fill-color: transparent;\nanimation: textclip 2s linear infinite;\ndisplay: block;\nbackground-image: url(https://up6.cc/2023/01/167396671537572.gif);\n-webkit-background-clip: text;\ncolor: transparent !important;\nbackground-size: 90% 100%;\n}\n.room.borderg.hand.nosel.fl.rbdu1t0locd {\n  border-radius: 5px;\n  background-image: url(https://girlspics.cc/wp-content/uploads/2018/08/6306.png);\n  background-size: 100%;\n  height: 140px;\n}\n.room.borderg.hand.nosel.fl.rbdu1t0locd .fl.u-pic {\n  border-radius: 4px!important;\n display:none; border: 2px solid #ffdbe3 !important;\n  letter-spacing: 1px;\n  background-size: 108% 108%;\n  -webkit-text-fill-color: white;\n  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;\n  min-width: 62px !important;\n  width: 62px !important;\n  min-height: 60px !important;\n  max-height: 60px !important;\n  margin-top: 4px;\n}\n.room.borderg.hand.nosel.fl.rbdu1t0locd .u-topic.ui-corner-all.dots {\ntext-transform: uppercase;\n-webkit-text-fill-color: transparent;\nanimation: textclip 2s linear infinite;\ndisplay: block;\nfont-size: 20px !important;\nbackground-image: url(https://girlspics.cc/wp-content/uploads/2018/08/6306.png);\n-webkit-background-clip: text;\ncolor: transparent !important;\nbackground-size: 100% 100%;\n\n}\n@keyframes textclip {to {background-position: 200% center;}}</style>'
+).insertBefore('body');
+
+var rid = 'bdu1t0locd';
+$('style:eq(0)').append(
+  '#d2 .roomh[onclick="Send_Rjoin(\'upoiguu32r\'' + rid + '\')"]{animation: textclip 5s linear infinite;\nborder-radius: 50px !important;\nborder: 1px solid #0000;\nbackground-image: url(https://up6.cc/2023/01/167396671537572.gif);\n-webkit-background-clip: text;\ncolor: transparent !important;\nbackground-size: 100% 100%;\nmargin-top: -3px;}'
+);
+$('style:eq(0)').append(
+  '#upro .roomh[onclick="Send_Rjoin(\'upoiguu32r\'' + rid + '\')"]{padding: 2px !important;\nanimation: textclip 5s linear infinite;\nborder-radius: 50px !important;\nborder: 1px solid #0000;\nbackground-image: url(https://up6.cc/2023/01/167396671537572.gif);\n-webkit-background-clip: text;\ncolor: transparent !important;\nbackground-size: 100% 100%;\nmargin-top: -5px;}'
+);
+$(
+  '<style>.fitimg.prod.mokt-lfon3{border: 2px solid;border-image: linear-gradient(var(--angle), #ff305d 0%, #000 29%, #ff2957 67%, #020202 100%) 1;animation: 5s rotate linear infinite;--angle: 0deg;background-color: #e3e3e3;border-radius: 5px !important;margin: 2px;margin-top: 1px;box-shadow: inset 0 0px 0px rgb(0 0 0 / 8%), 0 0px 1px #ffffff;height: 48px;}@keyframes rotate {\n    to {\n        --angle: 360deg;\n    }\n}\n@property --angle {\n    syntax: "<angle>";\n    initial-value: 0deg;\n    inherits: false;\n}</style>'
+).insertBefore('body');
+
+$(
+  '<style>.broadcasters.shmok1{ background-color: #5E5E5E !important; }.fitimg.prod.shmok1{margin-right: 0px!important;border: 2px solid;border-image: linear-gradient(var(--angle), #ffd6e0 0%, #000 29%, #ffd6e0 67%, #020202 100%) 1;animation: 5s rotate linear infinite;--angle: 0deg;background-color: #e3e3e3;border-radius: 5px !important;margin: 2px;margin-top: 1px;box-shadow: inset 0 0px 0px rgb(0 0 0 / 8%), 0 0px 1px #ffffff;height: 48px;}\n@keyframes rotate {\n\tto {\n\t\t--angle: 360deg;\n\t}\n}\n@property --angle {\n\tsyntax: "<angle>";\n\tinitial-value: 0deg;\n\tinherits: false;\n}</style>'
+).insertBefore('body');
+
+$('style').last().append(
+  '\n\n\n\n#upro.shmok1 .border {\n  border-radius: 0 10px 0 10px !important;\n  border: 1px solid #bfbfbf;\n}\n\n#upro.shmok1 .modal-header.label-primary
