403621ab7212e934142f20c8a6ab65378ee9ea19
dimask98 <56661641+dimask98@users.noreply.github.com>
2024-08-29 12:57:32 +0300

    Create CopyrightsAdhom.js

---
+++ Copyrights.js
@@ -0,0 +1,32 @@
+
+
+$('.fl.ustat')
+
+.css('width', '3px'), $('.fa.fa-user-plus')
+
+.html(`❥ تسجيل عضوية `), document.getElementById('u1')
+
+.placeholder = 'اكتب أي اسم من 8 احرف فقط ', 
+
+document.getElementById('pass1')
+
+.placeholder = 'كلمة المرور', document.getElementById('u2')
+
+.placeholder = 'فخآمة طلتك تنورنا', document.getElementById('pass2')
+
+.placeholder = ' ㋡ بـاسوردك ولاتنساه يـ جميل ', document.getElementById('u3')
+
+.placeholder = '♥ أكتب أي اسم من 8 أحرف فقط';
+
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span> لـوحة الشـآت ") 
+
+$(".fa-gear").text("آعدآدت الشات").css("width","");
+
+setTimeout (function (){  $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>هندسة و تصميم: <a href='https://ihtc0.com/'>  الدمشقي / نــورس © 2020</a></div>").insertAfter($(".loginstat"));  },1000);
+
+$("div#tlogins input").attr("maxlength",8),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
+
+$(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>دمـشقي </span></a>-->
+<a href="https://ihtc0.com/" target="_blank" style="width:107px; margin-right: 5px;
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >تصميم :  آلہـَدمـشۣۗہـقۗـۙيـہ 🕊❥⌁ / نــورس الــسوري  /a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
