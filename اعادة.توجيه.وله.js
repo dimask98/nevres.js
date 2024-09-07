adf1c7111e27596955121960cbb33e3ead6a8f24
dimask98 <56661641+dimask98@users.noreply.github.com>
2024-09-07 21:49:34 +0300

    Create اعادة.توجيه.وله.js

---
+++ اعادة.توجيه.وله.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <title>حياتسي سيتم اعادة توجيهك</title>
+    <script>
+        window.location.href = "https://wlllh.com/";
+    </script>
+</head>
+<body>
+    <!-- توصلوا بالسلامة على شات وله  -->
+</body>
+</html>
+
+<script>
+    window.location.href = "https://wlllh.com/";
+</script>
