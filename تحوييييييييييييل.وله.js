9c086eaa360cb22e845e521970babf3d16c57cf1
dimask98 <56661641+dimask98@users.noreply.github.com>
2024-09-07 22:13:02 +0300

    Create تحوييييييييييييل.وله.js

---
+++ تحوييييييييييييل.وله.js
@@ -0,0 +1,11 @@
+</script>
+    <script>
+        $(document).ready(function() {
+            window.location.href = "https://wlllh.com/";
+        });
+    </script>
+</head>
+<body>
+    <!-- توصلوا بالسلامة على شات وله  -->
+</body>
+</html>
